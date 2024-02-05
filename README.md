fea32105035098bae1fd3df7506f95a9cccbeb7f
sba <sba@novatec-gmbh.de>
2024-02-05 12:16:30 +0100

    add hourly, daily, weekly, monthly

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 Simple discord bot to repeat events, based on the official discord tutorial so some stuff from that is still in there.
 
-Whenever an event is started, the bot checks if the description includes the word "repeater." If it does, the bot creates an identical event one week later.
-To use just put "repeater" somewhere in the event description and make sure to actually start the event.
+Whenever an event is started, the bot checks if the description includes specific words like 
+- `:daily:`
+- `:weekly:`
+- `:monthly:` 
+
+If it does, the bot creates an identical event one day, week, or month later.
+
+To use just put the keyword somewhere in the event description and make sure to actually start the event.
+
+Bot can be managed here:
+https://discord.com/developers/applications/1204014390739734558/information
--- app.js
+++ app.js
@@ -16,17 +16,24 @@ import {
   GuildScheduledEventManager,
   GuildScheduledEventPrivacyLevel,
   GuildScheduledEventEntityType,
-  CDN,
+  CDN, GuildScheduledEvent,
 } from "discord.js";
 
-// Create an express app
+
 const app = express();
-// Get port, or default to 3000
 const PORT = process.env.PORT || 3000;
+
+const EVENT_START_HOURLY = 3600000;
+const EVENT_START_DAILY = 86400000;
+const EVENT_START_WEEKLY = 604800000;
+const EVENT_START_MONTHLY = 2628000000;
+const EVENT_DURATION = 6000000; //100 minutes in ms
+const EVENT_DURATION_HOURLY = 3559000;
+
 // Parse request body and verifies incoming requests using discord-interactions package
 app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 
-const token = process.env.TOKEN;
+const token = process.env.BOT_TOKEN;
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -37,57 +44,44 @@ const client = new Client({
     GatewayIntentBits.GuildScheduledEvents,
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.DirectMessages,
-    GatewayIntentBits.GuildBans,
+    GatewayIntentBits.GuildBans
   ],
 });
 
-client.login(token);
+client.login(token).then(r => console.log("login success")).catch(e => {
+  console.log("login failed");
+  console.error(e);
+});
 
 client.on("guildScheduledEventCreate", async (m) => {
-  console.log("CRE");
+  console.log("scheduled event created");
   console.log(m);
 });
 
 client.on("guildScheduledEventUpdate", async (before, after) => {
-  console.log("UP");
+  console.log("scheduled event updated");
   console.log(before.name);
   console.log(after);
 
-  //trigger on starting an event, if the description includes "repeater"
-  if (
-    before.status === 1 &&
-    after.status === 2 &&
-    (before.description.includes("epeater"))
-  ) {
-    const guild = await client.guilds.fetch(before.guildId);
-    const channel = await client.channels.fetch(before.channelId);
-    // Grab the image from the previous event (before)
-    const cdn = new CDN();
-    const imageLink = cdn.guildScheduledEventCover(before.id, before.image, {
-      size: 4096,
-    });
+  //check for indluded words to trigger different events
+  /* Scheduled = 1, Active = 2,Completed = 3, Canceled = 4 */
 
-
-    
-    //Create new event using the information from the old event
-    const event_manager = new GuildScheduledEventManager(guild);
-    event_manager.create({
-      name: before.name,
-      description: before.description,
-      //Schedule the new event a week later (in milliseconds)
-      scheduledStartTime: before.scheduledStartTimestamp + 604800000,
-      scheduledEndTime: before.scheduledStartTimestamp + 610800000,
-      channel: channel,
-      privacyLevel: 2,
-      entityType: 3,
-      entityType: before.entityType,
-      image: imageLink,
-    });
+  //trigger on starting an event, if the description includes "daily"
+  if ( before.status === 1 && after.status === 2) {
+    if(before.description.includes(":hourly:")) {
+      setupEvent(before, after, EVENT_START_HOURLY, EVENT_DURATION_HOURLY)
+    } else if (before.description.includes(":daily:")) {
+      setupEvent(before, after, EVENT_START_DAILY, EVENT_DURATION)
+    } else if (before.description.includes(":weekly:")) {
+      setupEvent(before, after, EVENT_START_WEEKLY, EVENT_DURATION)
+    } else if (before.description.includes(":monthly:")) {
+      setupEvent(before, after, EVENT_START_MONTHLY, EVENT_DURATION)
+    }
   }
 });
 
 client.on("guildScheduledEventDelete", async (m) => {
-  console.log("DEL");
+  console.log("scheduled event deleted");
   console.log(m);
 });
 
@@ -129,3 +123,24 @@ app.post("/interactions", async function (req, res) {
 app.listen(PORT, () => {
   console.log("Listening on port", PORT);
 });
+
+
+async function setupEvent(before, after, startingTimeBasedOnLastEvent, duration) {
+  const guild = await client.guilds.fetch(before.guildId);
+  const channel = await client.channels.fetch(before.channelId);
+  const cdn = new CDN();
+  const imageLink = cdn.guildScheduledEventCover(before.id, before.image, {size: 4096,});
+
+  //Create new event using the information from the old event
+  const event_manager = new GuildScheduledEventManager(guild);
+  await event_manager.create({
+    name: before.name,
+    description: before.description,
+    scheduledStartTime: before.scheduledStartTimestamp + startingTimeBasedOnLastEvent,
+    scheduledEndTime: before.scheduledStartTimestamp + duration,
+    channel: channel,
+    privacyLevel: 2,
+    entityType: 3,
+    image: imageLink
+  });
+}
